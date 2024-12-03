bb2cc0e3ca65e86a44d3b71ab29e4638e210177e
Radu <mihairadu8039@gmail.com>
2024-12-03 20:00:24 +0200

    Added the functionality

--- open-modal/main.js
+++ open-modal/main.js
@@ -4,15 +4,32 @@
 const openModalBtns = document.querySelectorAll(".btn__open--modal");
 const closeModalBtn = document.querySelector(".btn__close--modal");
 const overlayEl = document.querySelector(".overlay");
+const modalEl = document.querySelector(".modal");
 
 // Open modal
 openModalBtns.forEach((btn) =>
   btn.addEventListener("click", () => {
     overlayEl.classList.remove("hidden");
+    modalEl.classList.remove("hidden");
   })
 );
 
 // Close modal
 closeModalBtn.addEventListener("click", () => {
   overlayEl.classList.add("hidden");
+  modalEl.classList.add("hidden");
+});
+
+overlayEl.addEventListener("click", () => {
+  overlayEl.classList.add("hidden");
+  modalEl.classList.add("hidden");
+});
+
+// Close modal using Esc button
+document.addEventListener("keydown", (e) => {
+  // console.log(e);
+  if (e.key === "Escape") {
+    overlayEl.classList.add("hidden");
+    modalEl.classList.add("hidden");
+  }
 });
