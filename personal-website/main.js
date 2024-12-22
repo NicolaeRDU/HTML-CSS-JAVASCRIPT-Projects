0e12b0255c2f0a793209902249fab5ae2b831422
Radu <mihairadu8039@gmail.com>
2024-12-22 21:42:58 +0200

    Functionality for sections

--- personal-website/main.js
+++ personal-website/main.js
@@ -2,20 +2,36 @@
 
 // - logo one piece la projects /// optional
 
-// - o noua sectiune de "INTRO" dupa navbar
-
 "use strict";
+
+// Intro
+const scrollDown = document.querySelector(".scrollDown");
 
 // Main section
 const section2 = document.querySelector("#section--2");
 const goToProjects = document.querySelector(".main__btn");
+const typedTextSpan = document.querySelector(".typed-text");
+
+const words = [
+  "a Junior Web Developer",
+  "a chill guy",
+  "a gamer",
+  "a weeb",
+  "an One Piece fan",
+];
+const typingDelay = 100;
+const erasingDelay = 100;
+// Delay between current and next text
+const newLetterDelay = 1000;
+let index = 0;
+let charIndex = 0;
 
 // Projects section
 const tabs = document.querySelectorAll(".projects__tab");
 const tabsContainer = document.querySelector(".projects__tab--container");
 const tabsContent = document.querySelectorAll(".projects__content");
 
-// ////////////
+// PROJECTS tab ////////////
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".projects__tab");
 
@@ -31,12 +47,12 @@ tabsContainer.addEventListener("click", function (e) {
     .classList.add("projects__content--active");
 });
 
-// //////////////
+// MAIN to PROJECTS
 goToProjects.addEventListener("click", function () {
   section2.scrollIntoView({ behavior: "smooth" });
 });
 
-// //////////
+// NAV links logic //////////
 document.querySelector(".nav__list").addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -46,3 +62,48 @@ document.querySelector(".nav__list").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Go to MAIN
+document.querySelector(".begin").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  document.querySelector("#section--1").scrollIntoView({ behavior: "smooth" });
+});
+
+// Show go to MAIN
+setTimeout(function () {
+  scrollDown.classList.remove("hidden");
+}, 1500);
+
+// Added dynamic words
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(function () {
+    if (words.length) {
+      setTimeout(type, newLetterDelay);
+    }
+  }, 5000);
+});
+
+function type() {
+  if (charIndex < words[index].length) {
+    typedTextSpan.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.length) {
+      index = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
+}
