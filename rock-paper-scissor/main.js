8b2cfea476720c9c5d6491fff12664f110f41ced
Radu <mihairadu8039@gmail.com>
2025-01-03 23:32:29 +0200

    Added a score board

--- rock-paper-scissor/main.js
+++ rock-paper-scissor/main.js
@@ -5,8 +5,12 @@ const paperBtn = document.querySelector(".paper");
 const scissorBtn = document.querySelector(".scissor");
 const text = document.querySelector(".text");
 
+const winSection = document.querySelector(".win");
+const tieSection = document.querySelector(".tie");
+const looseSection = document.querySelector(".loose");
+
 // Computer choose a number
-const generateValue = function () {
+function generateValue() {
   let randomNumber = Number(Math.random().toFixed(2));
   let valueAI = "";
   if (randomNumber < 0.33) valueAI = "rock";
@@ -14,40 +18,75 @@ const generateValue = function () {
   if (0.66 <= randomNumber) valueAI = "scissor";
 
   return valueAI;
-};
+}
 
-// ROCK BTN
-rockBtn.addEventListener("click", function () {
-  text.classList.remove("hidden");
+let win = 0;
+let tie = 0;
+let loose = 0;
 
-  const valueAI = generateValue();
-  const valuePlayer = rockBtn.value;
-
-  if (valueAI === "rock") console.log("Tie");
-  if (valueAI === "paper") console.log("You loose");
-  if (valueAI === "scissor") console.log("You win");
-});
-
-// PAPER BTN
-paperBtn.addEventListener("click", function () {
+// play game function
+function playGame(value) {
   const valueAI = generateValue();
   let result = "";
   text.classList.remove("hidden");
 
-  if (valueAI === "rock") result = `Computer choose ${valueAI}; You win`;
-  if (valueAI === "paper") result = `Computer choose ${valueAI}; Tie`;
-  if (valueAI === "scissor") result = `Computer choose ${valueAI}; You loose`;
+  if (value === "rock") {
+    if (valueAI === value) {
+      result = `Computer choose ${valueAI}; Tie`;
+      tie++;
+    } else if (valueAI !== value) {
+      valueAI === "paper"
+        ? (result = `Computer choose ${valueAI}; You loose`)
+        : (result = `Computer choose ${valueAI}; You win`);
+
+      valueAI === "paper" ? loose++ : win++;
+    }
+  }
+
+  if (value === "paper") {
+    if (valueAI === value) {
+      result = `Computer choose ${valueAI}; Tie`;
+      tie++;
+    } else if (valueAI !== value) {
+      valueAI === "rock"
+        ? (result = `Computer choose ${valueAI}; You win`)
+        : (result = `Computer choose ${valueAI}; You loose`);
+
+      valueAI === "rock" ? win++ : loose++;
+    }
+  }
+
+  if (value === "scissor") {
+    if (valueAI === value) {
+      result = `Computer choose ${valueAI}; Tie`;
+      tie++;
+    } else if (valueAI !== value) {
+      valueAI === "rock"
+        ? (result = `Computer choose ${valueAI}; You loose`)
+        : (result = `Computer choose ${valueAI}; You win`);
+
+      valueAI === "rock" ? loose++ : win++;
+    }
+  }
 
   text.innerHTML = result;
+
+  winSection.textContent = win;
+  looseSection.textContent = loose;
+  tieSection.textContent = tie;
+}
+
+// ROCK BTN
+rockBtn.addEventListener("click", function () {
+  playGame(rockBtn.value);
+});
+
+// PAPER BTN
+paperBtn.addEventListener("click", function () {
+  playGame(paperBtn.value);
 });
 
 // SCISSOR BTN
 scissorBtn.addEventListener("click", function () {
-  text.classList.remove("hidden");
-  let valuePlayer = scissorBtn.value;
-  const valueAI = generateValue();
-
-  if (valueAI === "rock") console.log("You loose");
-  if (valueAI === "paper") console.log("You win");
-  if (valueAI === "scissor") console.log("Tie");
+  playGame(scissorBtn.value);
 });
